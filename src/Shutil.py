34fb33bc9c610fee2715186a06bc07239329d9ce
Balan Gnanam <balantgnanam@gmail.com>
2023-05-22 16:49:47 -0500

    Update Shutil.py

--- src/Shutil.py
+++ src/Shutil.py
@@ -54,3 +54,35 @@ s3_directory = 'path/to/s3/directory'
 
 # Upload the directory to S3
 upload_directory(local_directory, bucket_name, s3_directory)
+import os
+import pandas as pd
+
+def convert_csv_to_parquet(csv_path):
+    # Read the CSV file into a pandas DataFrame
+    df = pd.read_csv(csv_path)
+
+    # Extract the file name without extension
+    file_name = os.path.splitext(csv_path)[0]
+
+    # Convert and save the DataFrame as Parquet
+    parquet_path = file_name + '.parquet'
+    df.to_parquet(parquet_path)
+    print(f"Converted {csv_path} to {parquet_path}")
+
+def convert_csvs_to_parquet(directory):
+    # Iterate over all files and directories in the given directory
+    for root, dirs, files in os.walk(directory):
+        for file in files:
+            # Check if the file is a CSV
+            if file.endswith('.csv'):
+                # Get the full path of the CSV file
+                csv_path = os.path.join(root, file)
+                
+                # Convert the CSV file to Parquet
+                convert_csv_to_parquet(csv_path)
+
+# Specify the directory where the CSV files are located
+directory_path = '/path/to/csv/files'
+
+# Convert all CSV files in the directory and its subdirectories to Parquet
+convert_csvs_to_parquet(directory_path)
