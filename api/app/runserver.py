2f24b9eb3925410786954a924e500ca9978482b7
Balan Gnanam <balantgnanam@gmail.com>
2025-06-02 07:56:51 -0500

    Update runserver.py

--- api/app/runserver.py
+++ api/app/runserver.py
@@ -1,5 +1,17 @@
-mapRoles: |
-  - rolearn: arn:aws:iam::<account-id>:role/<role-name>
-    username: <role-name>
-    groups:
-      - eks-console-dashboard-viewer
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  name: aws-auth
+  namespace: kube-system
+data:
+  mapRoles: |
+    - rolearn: arn:aws:iam::<account-id>:role/<role-name>
+      username: <role-name>
+      groups:
+        - system:masters
+
+  mapUsers: |
+    - userarn: arn:aws:iam::<account-id>:user/<username>
+      username: <username>
+      groups:
+        - system:masters
