fc19b2560594d6955532571396b80a8f419938eb
Balan Gnanam <balantgnanam@gmail.com>
2025-06-02 23:23:35 -0500

    Update runserver.py

--- api/app/runserver.py
+++ api/app/runserver.py
@@ -1,14 +1,12 @@
 FROM amazonlinux:2
 
 RUN yum -y update && \
-    yum -y install gcc gcc-c++ make libcurl-devel openssl-devel libxml2-devel \
-                   wget tar which unzip less && \
+    yum -y install gcc gcc-c++ make wget tar gzip \
+                   libcurl-devel openssl-devel libxml2-devel && \
     yum clean all
 
-# Install R 4.0.5 manually
+# Download and install R
 RUN curl -O https://cran.r-project.org/src/base/R-4/R-4.0.5.tar.gz && \
     tar -xf R-4.0.5.tar.gz && \
-    cd R-4.0.5 && ./configure --with-x=no --enable-R-shlib && make -j4 && make install
-
-# Install R packages
-RUN Rscript -e 'install.packages(c("ggplot2", "dplyr"), repos="https://cloud.r-project.org")'
+    cd R-4.0.5 && ./configure --with-x=no --enable-R-shlib && \
+    make -j4 && make install
