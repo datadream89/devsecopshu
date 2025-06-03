da0ab691cb27747b80f7434049215727fb567742
Balan Gnanam <balantgnanam@gmail.com>
2025-06-02 23:28:51 -0500

    Update runserver.py

--- api/app/runserver.py
+++ api/app/runserver.py
@@ -1,12 +1,21 @@
 FROM amazonlinux:2
 
+# Install dependencies, including Fortran compiler
 RUN yum -y update && \
-    yum -y install gcc gcc-c++ make wget tar gzip \
-                   libcurl-devel openssl-devel libxml2-devel && \
+    yum -y install \
+        gcc gcc-c++ gcc-gfortran make \
+        wget tar gzip \
+        libcurl-devel openssl-devel libxml2-devel \
+        readline-devel zlib-devel bzip2-devel xz-devel && \
     yum clean all
 
-# Download and install R
+# Install R 4.0.5
 RUN curl -O https://cran.r-project.org/src/base/R-4/R-4.0.5.tar.gz && \
     tar -xf R-4.0.5.tar.gz && \
-    cd R-4.0.5 && ./configure --with-x=no --enable-R-shlib && \
-    make -j4 && make install
+    cd R-4.0.5 && \
+    ./configure --enable-R-shlib --with-x=no && \
+    make -j4 && \
+    make install
+
+# Test installation
+RUN Rscript -e 'version'
